da58a7d0373aae11446bb33bc2da35ab0cfb6fee
Secret Specter <talon@theghostin.me>
2018-09-19 16:08:45 -0400

    now that's a radio player

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,11 @@
 import kotlinx.html.*
+import kotlinx.html.js.onEndedFunction
 import me.theghostin.nimble.app
 import me.theghostin.nimble.html
-import kotlin.browser.window
+import org.w3c.dom.HTMLAudioElement
 
 data class Model(
-        val station_url: String = "/pirate-radio",
+        val station_url: String = "/pirate-radio/station",
         val station: Station = Station()
 )
 
@@ -12,24 +13,23 @@ data class Station(
         val songs_url: String = "/songs",
         val title: String = "Pirate Radio",
         val summary: String = "radio for <b>pirates</b>",
-        val artwork: String? = null
+        val artwork: String? = "artwork.gif"
 )
 
-
-data class Song(
-        val file: String,
-        val title: String = "untitled",
-        val artist: String = "anonymous",
-        val album: String? = null,
-        val year: Int? = null,
-        val comment: String? = null,
-        val artwork: String? = null
-)
+// data class Song(
+//         val file: String,
+//         val title: String = "untitled",
+//         val artist: String = "anonymous",
+//         val album: String? = null,
+//         val year: Int? = null,
+//         val comment: String? = null,
+//         val artwork: String? = null
+// )
 
 sealed class Msg
 
 fun main(args: Array<String>) {
-    app<Msg, Model>(Model("/pirate-radio/station/")) {
+    app<Msg, Model>(Model()) {
         // fetch and display station data
         // window.fetch("${model.station}/meta.json").then { it.text() }
         //         .then { json -> JSON.parse<Station>(json).apply {
@@ -42,13 +42,30 @@ fun main(args: Array<String>) {
         //                     } }
         //        } }
         html {
-            img(src = "${model.station_url}/artwork.gif") {}
-            audio {
-                src = "${model.station_url}/songs/audio/mansion.wav"
-                autoPlay = true
-            }
+            img(src = "${model.station_url}${model.station.artwork}") {}
+            radio(listOf(
+                    "mansion.wav",
+                    "somewhere.wav"
+            ).map {
+                "${model.station_url}${model.station.songs_url}/audio/$it"
+            })
             h1 { +model.station.title }
             p { unsafe { +model.station.summary } }
+        }
+    }
+}
+
+fun DIV.radio(songs: List<String>) {
+    var i = 0
+    audio {
+        src = songs[i]
+        autoPlay = true
+        onEndedFunction = {
+            i = (i + 1).let { when (it <= songs.size - 1) {
+                true -> it
+                false -> 0
+            } }
+            (it.target as HTMLAudioElement).src = songs[i]
         }
     }
 }

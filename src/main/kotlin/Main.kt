61c3882b359a023da023e169119514ff2bcfd227
Secret Specter <talon@theghostin.me>
2018-09-19 20:48:13 -0400

    almost wired to the pirate-radio.json but kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,74 +1,111 @@
 import kotlinx.html.*
 import kotlinx.html.js.onEndedFunction
+import me.theghostin.nimble.Nimble
 import me.theghostin.nimble.app
 import me.theghostin.nimble.html
 import org.w3c.dom.HTMLAudioElement
 import kotlin.browser.window
+import kotlin.js.Math
 
 data class Model(
-        val station_url: String = "/pirate-radio/station/",
-        val station: Station = Station(),
-        val songs: List<String> = listOf()
+        val station_url: String = "/pirate-radio/station",
+        val station: Station = Station()
 )
 
 data class Station(
-        val songs_url: String = "/songs",
         val title: String = "Pirate Radio",
-        val summary: String = "radio for <b>pirates</b>",
-        val artwork: String? = "artwork.gif"
+        val summary: String = "radio for <i>pirates<i>",
+        val artwork: String? =  "https://media.giphy.com/media/LUIvcbR6yytz2/giphy.gif",
+        val songs: Map<String, Song> = mapOf()
 )
 
-// data class Song(
-//         val file: String,
-//         val title: String = "untitled",
-//         val artist: String = "anonymous",
-//         val album: String? = null,
-//         val year: Int? = null,
-//         val comment: String? = null,
-//         val artwork: String? = null
-// )
+data class Song(
+        val title: String? = "untitled",
+        val artist: String? = "anonymous",
+        val album: String? = null,
+        val year: Int? = null,
+        val comment: String? = null,
+        val artwork: String? = null
+)
 
 sealed class Msg
-data class Songs(val songs: List<String>?) : Msg()
+data class SongsUpdate(val songs: Map<String, Song>) : Msg()
+data class StationUpdate(val station: Station) : Msg()
 
 fun main(args: Array<String>) {
     app<Msg, Model>(Model()) {
-        window.fetch("${model.station_url}${model.station.songs_url}/audio/").then { it.text() }
-                .then {
-                    // the endpoint returns html linking to the files, we're gonna scrape the file names from that.
-                    // find anything that starts with > and ends with .wav. (that's the link text)
-                    // map that to a list and remove the >
-                    send(Songs(Regex(">(.+).wav").findAll(it)
-                            .map { it.value.substring(1) }.toList()))
-                }
-
         inbox { when (it) {
-            is Songs -> model.copy(songs = it.songs ?: listOf())
+            is SongsUpdate -> model.copy(
+                station = model.station.copy(songs = it.songs)
+            )
+            is StationUpdate -> model.copy(
+                station = model.station.copy(
+                        title = it.station.title,
+                        summary = it.station.summary,
+                        artwork = it.station.artwork,
+                        songs = model.station.songs.plus(it.station.songs)
+                )
+            )
         } }
 
+        station(model.station_url)
+
         html {
-            img(src = "${model.station_url}${model.station.artwork}") {}
-            radio(model.songs.map {
-                "${model.station_url}${model.station.songs_url}/audio/$it"
-            })
+            console.log(model)
+            img(src = model.station.artwork) {}
+            radio(model.station.songs)
             h1 { +model.station.title }
             p { unsafe { +model.station.summary } }
         }
     }
 }
 
-fun DIV.radio(songs: List<String>) {
+fun Nimble<Msg, Model>.station(url: String) {
+    /*
+     * audio
+     */
+    window.fetch("$url/songs").then { it.text() }
+            .then { html ->
+                html.files("(mp3|wav)").map { filename ->
+                    "$url/songs/$filename" to Song()
+                }.toMap()
+            }
+            .then { send(SongsUpdate(it)) }
+            .catch { e -> console.log(e) }
+
+    /*
+     * meta data
+     */
+    // window.fetch("$url/pirate-radio.json").then { it.text() }
+    //         // TODO: I think switching to kotlinx will make this work but idk
+    //         .then { JSON.parse<Station>(it) }
+    //         .then { send(StationUpdate(it)) }
+    //         .catch { e -> console.log(e) }
+}
+
+fun DIV.radio(songs: Map<String, Song>) {
     if (songs.isEmpty()) return
-    var i = 0
+    var i = (0 until songs.size).random
     audio {
-        src = songs[i]
+        src = songs.keys.toList()[i]
         autoPlay = true
         onEndedFunction = {
             i = (i + 1).let { when (it <= songs.size - 1) {
                 true -> it
-                false -> 0
+                false -> (0 until songs.size).random
             } }
-            (it.target as HTMLAudioElement).src = songs[i]
+            (it.target as HTMLAudioElement).src = songs.keys.toList()[i]
         }
     }
 }
+
+private val IntRange.random: Int get() =
+    (Math.random() * (endInclusive - first) + first).toInt()
+
+// the endpoint returns html linking to the files, we're gonna scrape the file names from that.
+// find anything that starts with `>` and ends with the extension. (that's the link text)
+fun String.files(extension: String) = Regex(">(.+).$extension").findAll(this)
+        // remove the `>`
+        .map { it.value.substring(1) }
+
+
